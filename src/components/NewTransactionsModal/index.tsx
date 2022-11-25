fb2087a2bf1d3588a016c9043ceb75bbd434d3ad
jonasborges93 <jonas.borges93@hotmail.com>
2022-11-25 20:10:14 -0300

    Styling Modal Component

--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,5 +1,9 @@
 import Modal from 'react-modal';
 
+import closeImg from '../../assets/close.svg';
+
+import {Container} from './styles'
+
 interface NewTransactionModalProps{
     isOpen: boolean;
     onRequestClose: () => void;
@@ -10,8 +14,40 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <Modal 
           isOpen={isOpen}
           onRequestClose={onRequestClose}
+          overlayClassName="react-modal-overlay"
+          className="react-modal-content"
         >
-          <h2>Cadastrar transação</h2>
+            <button 
+                type="button" 
+                onClick={onRequestClose}
+                className="react-modal-close" 
+                >
+                <img src={closeImg} alt="Fechar Modal" />
+            </button>
+            <Container>
+                <h2>Cadastrar transação</h2>
+
+                <input 
+                    type="text"
+                    placeholder="Título"
+                />
+
+                <input 
+                    type="number"
+                    placeholder="Valor"
+                />
+
+                <input 
+                    type="text"
+                    placeholder="Categoria"
+                />
+
+                <button
+                    type="submit"
+                >
+                    Cadastrar
+                </button>
+            </Container>
         </Modal>
     );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -58,4 +58,46 @@ button{
     opacity: 0.6;
     cursor: not-allowed;
 }
+
+.react-modal-overlay{
+    background-color: rgba(0,0,0,0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.react-modal-content{
+    max-width: 576px;
+    width: 100%;
+
+    background-color: var(--background);
+    padding: 3rem;
+    border-radius: 0.25rem;
+
+    position: relative;
+
+}
+
+.react-modal-close{
+    position: absolute;
+
+    top: 1.5rem;
+    right: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover{
+        filter: brightness(0.8);
+    }
+
+}
 `
